b46ebbf1f333b99f7290d7f20e22e175e4859dab
Oluwaseun <adunmofestus@gmail.com>
2023-09-20 20:34:01 +0100

    alert box ADDED

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function myFunction() {
     document.getElementById("first").innerHTML = "Hello Dolly!";
 document.getElementById("second").innerHTML = "How are you?";
 }
+alert( 10 * 5);
 
+let text1 = "What a very ";
+text1 += "nice day";
+document.getElementById("demo").innerHTML = text1;
